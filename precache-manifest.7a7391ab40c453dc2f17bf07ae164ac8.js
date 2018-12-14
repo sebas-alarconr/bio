0e09c4765845f19700f3125bd4037089ea154953
Sebas Alarcón <sebas.alarcon8@gmail.com>
2018-12-14 16:31:50 -0500

    Updates

---
+++ precache-manifest.7a7391ab40c453dc2f17bf07ae164ac8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "34252642cbce0f840bda",
+    "url": "/bio/static/js/runtime~main.34252642.js"
+  },
+  {
+    "revision": "6b7076d940f1e0d2803f",
+    "url": "/bio/static/js/main.6b7076d9.chunk.js"
+  },
+  {
+    "revision": "df901aa2b289ad0d9b29",
+    "url": "/bio/static/js/1.df901aa2.chunk.js"
+  },
+  {
+    "revision": "6b7076d940f1e0d2803f",
+    "url": "/bio/static/css/main.08c713e1.chunk.css"
+  },
+  {
+    "revision": "e031ad294b158653e38f36782a5e52a4",
+    "url": "/bio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/bio/precache-manifest.69324f154bb778bcc7d7c2448448dd29.js"
+  "/bio/precache-manifest.7a7391ab40c453dc2f17bf07ae164ac8.js"
 );
 
 workbox.clientsClaim();

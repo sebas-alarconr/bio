e2e78b852de8255688b340f8d1c76e5fe8fbde33
Sebas Alarcón <sebas.alarcon8@gmail.com>
2018-12-14 16:48:29 -0500

    Configure React Router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
+import { Route, Switch, BrowserRouter} from 'react-router-dom';
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import IndexPage from './pages/IndexPage';
+import ResumePage from './pages/ResumePage';
+import NotFoundPage from './pages/NotFoundPage';
 
 class App extends Component {
   render() {
+    console.log('publicrl', process.env.PUBLIC_URL);
+    console.log('reactpubliclib', process.env.REACT_APP_PUBLIC_URL);
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter
+        basename={process.env.PUBLIC_URL || process.env.REACT_APP_PUBLIC_URL}
+      >
+        <Switch>
+          <Route exact path="/" component={IndexPage} />
+          <Route exact path="/resume" component={ResumePage} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/pages/ResumePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ResumePage extends Component {
+  render() {
+    return(
+      <div>
+        Resume Page
+      </div>
+    )
+  }
+}
+
+export default ResumePage;

5676955ec74eb91f23c957354f5d3f71f493727b
Sebastian Alarcon <sebas.alarcon8@gmail.com>
2019-05-15 16:04:57 -0500

    Integrating Firebase. Including some OG meta tags.

---
+++ src/components/Firebase.js
@@ -0,0 +1,27 @@
+import app from 'firebase/app'
+import 'firebase/firebase-firestore'
+
+const config = {
+  apiKey: "AIzaSyBSFtPxgJS8USawtuUnHwVHH0ttP3V7Nx8",
+  authDomain: "my-personal-bio.firebaseapp.com",
+  databaseURL: "https://my-personal-bio.firebaseio.com",
+  projectId: "my-personal-bio",
+  storageBucket: "my-personal-bio.appspot.com",
+  messagingSenderId: "770715025261",
+  appId: "1:770715025261:web:8aa851a469fa962f"
+}
+
+class Firebase {
+	constructor() {
+    app.initializeApp(config)
+
+    this.db = app.firestore();
+    this.bioId = 'dqqrhVhL4q05XfXYhP9R'
+	}
+
+	async getBio() {
+		return await this.db.doc(`bios/${this.bioId}`).get();
+	}
+}
+
+export default new Firebase()
--- src/pages/ResumePage.js
+++ src/pages/ResumePage.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
-import resumeData from '../data/resume';
+import firebase from '../components/Firebase';
+import _ from 'lodash';
 
 class ResumePage extends Component {
-  render = () => (
-    <article className="resume app__content">
-      <h1>Overview</h1>
-      <p>{resumeData.overview}</p>
-      <h2>Experience</h2>
-      <ul className="resume__experience">
-        {resumeData.experience.map(this.renderExperience)}
-      </ul>
-      <h2>Education</h2>
-      <ul className="resume__education">
-        {resumeData.education.map(this.renderEducation)}
-      </ul>
-      <h2>Community</h2>
-      <ul className="resume__community">
-        {resumeData.community.map(this.renderCommunity)}
-      </ul>
-    </article>
-  )
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      bioData: {}
+    }
+  }
+
+  componentDidMount = () => {
+    firebase.getBio().then(bio => this.setState({bioData: bio.data()}));
+  }
+
+  render = () => {
+    const { bioData } = this.state;
+
+    return (
+      <article className="resume app__content">
+        <h1>Overview</h1>
+        <p>{_.get(bioData, 'overview')}</p>
+        <h2>Experience</h2>
+        <ul className="resume__experience">
+          {_.get(bioData, 'experience', []).map(this.renderExperience)}
+        </ul>
+        <h2>Education</h2>
+        <ul className="resume__education">
+          {_.get(bioData, 'education', []).map(this.renderEducation)}
+        </ul>
+        <h2>Community</h2>
+        <ul className="resume__community">
+          {_.get(bioData, 'community', []).map(this.renderCommunity)}
+        </ul>
+      </article>
+    )
+  }
 
   renderExperience = (experienceItem, key) => (
     <li className="resume__experience-item" key={key}>
@@ -34,9 +51,9 @@ class ResumePage extends Component {
       <div className="resume__experience-item-content">
         <p>{experienceItem.title}</p>
         <p>{experienceItem.start} - {experienceItem.end}</p>
-        <p>{experienceItem.technologies}</p>
+        <p>{_.get(experienceItem, 'technologies', []).map(this.renderTechnologies)}</p>
         <ul>
-          {experienceItem.responsabilities.map(this.renderExtraInfo)}
+          {_.get(experienceItem, 'responsabilities', []).map(this.renderExtraInfo)}
         </ul>
       </div>
     </li>
@@ -49,14 +66,14 @@ class ResumePage extends Component {
           <div className="resume__list-bullet resume__list-bullet--inner"></div>
         </div>
         <h3>
-          {educationItem.company}
+          {educationItem.institution}
         </h3>
       </div>
       <div className="resume__education-item-content">
         <p>{educationItem.title}</p>
         <p>{educationItem.start} - {educationItem.end}</p>
         <ul>
-          {educationItem.extras.map(this.renderExtraInfo)}
+          {_.get(educationItem, 'extras', []).map(this.renderExtraInfo)}
         </ul>
       </div>
     </li>
@@ -74,9 +91,9 @@ class ResumePage extends Component {
       </div>
       <div className="resume__community-item-content">
         <p>{communityItem.description}</p>
-        <p>{communityItem.dates.map(this.renderDates)}</p>
+        <p>{_.get(communityItem, 'dates', []).map(this.renderDates)}</p>
         <ul>
-          {communityItem.events.map(this.renderEvents)}
+          {_.get(communityItem, 'events', []).map(this.renderEvents)}
         </ul>
       </div>
     </li>
@@ -98,10 +115,19 @@ class ResumePage extends Component {
     )
   }
 
+  renderTechnologies = (tech, key) => {
+    const complement = key ? ', ' : null;
+
+    return (
+      <span key={key}>
+        {complement}{tech}
+      </span>
+    )
+  }
+
   renderEvents = (event, key) => (
     <li className="resume__community-item-extra" key={key}>
-      <p>{event.title}</p>
-      <a href={event.link}>({event.link})</a>
+      {event.title} <a href={event.link.url} target="_blank" rel="noopener noreferrer">{event.link.text}</a>
     </li>
   )
 }

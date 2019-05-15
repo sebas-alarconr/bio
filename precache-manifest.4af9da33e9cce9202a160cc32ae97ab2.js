259814c9d198881edd45145a7aef0a4878245ed2
Sebastian Alarcon <sebas.alarcon8@gmail.com>
2019-05-15 15:29:33 -0500

    Updates

---
+++ precache-manifest.4af9da33e9cce9202a160cc32ae97ab2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b37c9edf651561e5e2fe",
+    "url": "/bio/static/css/main.7e9b28b7.chunk.css"
+  },
+  {
+    "revision": "b37c9edf651561e5e2fe",
+    "url": "/bio/static/js/main.b37c9edf.chunk.js"
+  },
+  {
+    "revision": "4278433b1dd4a622de51",
+    "url": "/bio/static/css/1.5e2f8aaf.chunk.css"
+  },
+  {
+    "revision": "4278433b1dd4a622de51",
+    "url": "/bio/static/js/1.4278433b.chunk.js"
+  },
+  {
+    "revision": "34252642cbce0f840bda",
+    "url": "/bio/static/js/runtime~main.34252642.js"
+  },
+  {
+    "revision": "039c6ddd716da7324ad2978fccadea1e",
+    "url": "/bio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/bio/precache-manifest.6d6ce4a83e9b81e15e807a58f5f16873.js"
+  "/bio/precache-manifest.4af9da33e9cce9202a160cc32ae97ab2.js"
 );
 
 workbox.clientsClaim();

93370ba047678509f39e0fe8ebdecafefbbc7b5e
sebas-alarconr <sebas.alarcon8@gmail.com>
2020-06-05 15:25:59 +0000

    Deploying to gh-pages from  @ 117280ebcac60b114dec7a8f58f9e0ce280e12ff 🚀

---
+++ precache-manifest.1348370eb561a3a7b92503bc1aab23c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd3499b2961bd40d8b6b4bf997ba7f6e",
+    "url": "/bio/index.html"
+  },
+  {
+    "revision": "d21eabbd8b00d6f280a3",
+    "url": "/bio/static/css/2.ad663270.chunk.css"
+  },
+  {
+    "revision": "5aac9dcd20d9bd591bb3",
+    "url": "/bio/static/css/main.32974ee9.chunk.css"
+  },
+  {
+    "revision": "d21eabbd8b00d6f280a3",
+    "url": "/bio/static/js/2.79fc1a57.chunk.js"
+  },
+  {
+    "revision": "5dd65c0ed37153ed098167c4e40ee931",
+    "url": "/bio/static/js/2.79fc1a57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aac9dcd20d9bd591bb3",
+    "url": "/bio/static/js/main.9b826f0f.chunk.js"
+  },
+  {
+    "revision": "bb0303f1b9f605c520da",
+    "url": "/bio/static/js/runtime-main.b2f01e1c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bio/precache-manifest.deb05f5d35dec12e5d60f915089b7a32.js"
+  "/bio/precache-manifest.1348370eb561a3a7b92503bc1aab23c2.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/bio/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
